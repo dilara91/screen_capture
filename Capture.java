6d0718a898fa6a0c57aac533ef7b796ad2e5ba08
dilara91 <dilara91@users.noreply.github.com>
2017-02-02 19:34:00 +0300

    Update Capture.java

--- Capture.java
+++ Capture.java
@@ -14,26 +14,26 @@ public class Capture {
 
 	public static void main(String[] args){
 		int count = 0;
-		int sleep = Integer.parseInt(args[0]);
-		int seconds = Integer.parseInt(args[1]);
+		int sleep = Integer.parseInt(args[0]); //duration of wait between screenshots in ms
+		int seconds = Integer.parseInt(args[1]); //duration of the screencapture in seconds 
 		int max_decimal = findDecimal(sleep,seconds);
 		
 		while(count<(1000.0/sleep)*seconds){
 			Toolkit toolkit = Toolkit.getDefaultToolkit();
 			Dimension screenSize = toolkit.getScreenSize();
 			Rectangle screenRect = new Rectangle(screenSize);
-			// create screen shot
+			
 			Robot robot;
 			try {
 				robot = new Robot();
 			
 			BufferedImage image = robot.createScreenCapture(screenRect);
-			// save captured image to PNG file
+			// save captured image to JPG file
 			ImageIO.write(image, "jpg", new File("screen_"+createName(count,max_decimal)+".jpg"));
 			count++;
 			Thread.sleep(sleep);
 			} catch (AWTException | IOException | InterruptedException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
 		}
@@ -42,7 +42,9 @@ public class Capture {
 		
 		
 	}
-	
+	/**
+	Finds the maximum number of frames
+	*/
 	public static int findDecimal(int sleep,int seconds){
 	
 		int max_count =(int)(1000.0/sleep)*seconds;
@@ -53,7 +55,9 @@ public class Capture {
 		}
 		return toReturn;
 	}
-	
+	/**
+	Creates image names with preceding zeros for ordering
+	*/
 	public static String createName(int count,int decimal){
 		int cur_decimal = 0;
 		int temp = count;
